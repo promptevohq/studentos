b04c5653bcc36472cee8a6ddec07f78f925c7606
promptevohq <krishdhanula540210@gmail.com>
2026-06-16 16:59:54 +0530

    add screenshots to manifest

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,9 @@ export default defineConfig({
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
+        ],
+        screenshots: [
+          { src: 'icon-512.png', sizes: '512x512', type: 'image/png', form_factor: 'narrow' }
         ]
       }
     })
